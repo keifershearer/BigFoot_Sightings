66d52f66695ca220adcdc87a9f6bf2fef2560939
TomMahe <mahettom@gmail.com>
2023-02-20 16:43:47 +0100

    add the firsts routes for sightings

--- routes/sighting.routes.js
+++ routes/sighting.routes.js
@@ -5,17 +5,37 @@ const Sighting = require('../models/Sighting.model')
 
 
 // RENDER ALL SIGHTINGS PAGE
-
+router.get('/sightings', async (req, res, next) => {
+    try {
+        const allSightings = await Sighting.find()
+        res.render('sighting/sightings', allSightings)
+    } catch (err) {
+        next(err)
+    }
+})
 
 
 
 // RENDER PAGE FOR CREATE A SIGHTING
-
-// —————————————————————————————————————————————————> MAKE A FORM IN THE VIEW FILE WE RENDER
-
+router.get('/create-sighting', async (req, res, next) => {
+    try {
+        res.render('sighting/sightings')
+    } catch (err) {
+        next(err)
+    }
+})
 
 
 // LISTEN FOR A FORM ON THE CREATE SIGHTING PAGE 
+router.post('/create-sighting', async (req, res, next) => {
+    try {
+        const { location, description, date, owner } = req.body
+        const newSighting = await Sighting.create({ location, description, date, owner })
+        res.redirect('sighting/sightings', newSighting)
+    } catch (error) {
+        next(err)
+    }
+})
 
 // ————————————————————————————————————————————————> ACCES INFORMATION USER JUST PROVIDE WITH req.body
 // ————————————————————————————————————————————————> CREATE THE SIGHTING WIHT      Sighting.create()
