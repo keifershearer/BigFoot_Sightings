96161dc39464026442da95f3cba082145eae8ca3
bef4 <befacool@gmail.com>
2023-02-20 11:47:27 +0100

    just render login/main

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,21 +4,23 @@ const User = require('../models/User.model')
 const bcrypt = require('bcryptjs')
 
 
-
-
 // CREATE ROUTE FOR LOGIN     GET
-
-
-
-
+router.get('/login', (req, res, next) => {
+    try {
+        res.render('auth/login')
+    } catch (err) {
+        next(err)
+    }
+})
 
 // CREATE ROUTE FOR SIGN UP    GET
-
-
-
-
-
-
+router.get('/signup', (req, res, next) => {
+    try {
+        res.render('auth/signup')
+    } catch (err) {
+        next(err)
+    }
+})
 
 // LISTEN FOR A POST FORM ON THE 'LOGIN' ROUTE
 
