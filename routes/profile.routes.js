89386d3e594d43a12c5bb28b506ed7e8ca6fe8bf
Keifer Shearer <keifearer@gmail.com>
2023-02-21 10:20:54 +0100

    render profile and profile view

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Sighting = require('../models/Sighting.model')
 const User = require('../models/User.model')
+const isLogin = require()
 
 
 
 // RENDER PROFILE PAGE —————>  /
-router.get('/:profileId', async (res, req, next) => {
-
-    const user = await User.findById(req.params.profileId)
+router.get('/:profileId', isLogin, (res, req, next) => {
     res.render('profile', { user })
 })
 
